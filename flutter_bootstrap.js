1e74ea69eb448228652d56bcb4f9cdb8f2c901bf
playnconnect <playnconnect.team@gmail.com>
2025-04-22 14:27:54 +0200

    to continue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "490874385"
+    serviceWorkerVersion: "2778405544"
   }
 });
